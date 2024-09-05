c1f750725250853269398289e8fd00089f2736be
Coder <dmwgullcoder@gmail.com>
2024-09-05 18:23:11 +0300

    ISSUE#: SEC-005: Устаеовка SDL2. Description: Редакция read_me, подключение библиотек.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
+#include <SDL.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL не удалось инициализировать! SDL Ошибка: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Пример SDL в CLion", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "Не удалось создать окно! SDL Ошибка: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000); // Задержка на 3 секунды
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
